6831c41ffaff87c714922dce917cdd14d180e887
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-01-05 20:48:38 -0600

    Add follow behavior

--- catkin_ws/src/eir2023/controllers/supervisor_free_north/supervisor_free_north.py
+++ catkin_ws/src/eir2023/controllers/supervisor_free_north/supervisor_free_north.py
@@ -7,7 +7,12 @@ from controller import Supervisor
 TIME_STEP = 10
 robot = Supervisor()
 
+def callback_start(msg):
+    global start
+    start = True
+
 def main():
+    global start
     print('Starting Controller Supervisor...')
     car_1 = robot.getFromDef('obstacle_north_west')
     car_2 = robot.getFromDef('obstacle_west')
@@ -18,16 +23,16 @@ def main():
     tf_1 = car_1.getField('translation')
     tf_2 = car_2.getField('translation')
     tf_3 = car_3.getField('translation')
-
+    start = False
     rospy.init_node("supervisor_node")
-    # print("Supervisor.->Waiting for start signal")
-    # rospy.wait_for_message("/start", Empty)
-    # print("Supervisor.->Start signal received")
-    # car_1.setVelocity([6,0,0, 0,0,0])
-    # car_2.setVelocity([6,0,0, 0,0,0])
-    # car_3.setVelocity([6,0,0, 0,0,0])
+    rospy.Subscriber("/start", Empty, callback_start)
     loop = rospy.Rate(1000/TIME_STEP)
     while robot.step(TIME_STEP) != -1 and not rospy.is_shutdown():
+        if start:
+            start = False
+            car_1.setVelocity([6,0,0, 0,0,0])
+            car_2.setVelocity([6,0,0, 0,0,0])
+            car_3.setVelocity([6,0,0, 0,0,0])
         loop.sleep()
         
 if __name__ == "__main__":
--- catkin_ws/src/eir2023/scripts/lane_tracking_control_P.py
+++ catkin_ws/src/eir2023/scripts/lane_tracking_control_P.py
@@ -10,7 +10,7 @@ set as a constant.
 import cv2
 import numpy
 import rospy
-from std_msgs.msg import Float64MultiArray, Float64, Empty
+from std_msgs.msg import Float64MultiArray, Float64, Empty, Bool
 
 #
 # Steering is calculated proportional to two errors: distance error and angle error.
@@ -19,7 +19,7 @@ from std_msgs.msg import Float64MultiArray, Float64, Empty
 # Speed is calculated as the max speed minus a speed proportional to the steering.
 # In this way, car goes with lower speed in curves and at max speed in straight roads. 
 #
-def calculate_control(rho_l, theta_l, rho_r, theta_r, goal_rho_l, goal_theta_l, goal_rho_r, goal_theta_r):
+def calculate_control(rho_l, theta_l, rho_r, theta_r, goal_rho_l, goal_theta_l, goal_rho_r, goal_theta_r, dist=None):
     global max_speed, k_rho, k_theta
     error_rho_l   = goal_rho_l   - rho_l
     error_theta_l = goal_theta_l - theta_l
@@ -36,7 +36,10 @@ def calculate_control(rho_l, theta_l, rho_r, theta_r, goal_rho_l, goal_theta_l, 
         error_theta = error_theta_r
     
     steering = -k_rho*error_rho - k_theta*error_theta
-    speed = max_speed*(1 - 1.5*abs(steering))
+    if dist is None:
+        speed = max_speed*(1 - 1.5*abs(steering))
+    else:
+        speed = 36 + 10*(9 - dist)
     return speed, steering
 
 def callback_left_lane(msg):
@@ -47,9 +50,22 @@ def callback_right_lane(msg):
     global lane_rho_r, lane_theta_r
     lane_rho_r, lane_theta_r = msg.data
 
+def callback_enable_steady_motion(msg):
+    global enable_steady_motion
+    enable_steady_motion = msg.data
+
+def callback_enable_follow(msg):
+    global enable_follow
+    enable_follow = msg.data
+
+def callback_dist_to_obstacle(msg):
+    global dist_to_obstacle
+    dist_to_obstacle = msg.data
+
 def main():
     global lane_rho_l, lane_theta_l, lane_rho_r, lane_theta_r
     global max_speed, k_rho, k_theta
+    global enable_steady_motion, enable_follow, dist_to_obstacle
     max_speed = 10
     k_rho   = 0.001
     k_theta = 0.01
@@ -71,13 +87,12 @@ def main():
     if rospy.has_param('~k_theta'):
         k_theta = rospy.get_param('k_theta')
 
-    print("LaneTracking.->Waiting for start signal")
-    rospy.wait_for_message("/start", Empty, timeout=10000.0)
-    print("LaneTraking.->Start signal received")
-
     print("Waiting for lane detection...")
     rospy.Subscriber("/demo/left_lane" , Float64MultiArray, callback_left_lane)
     rospy.Subscriber("/demo/right_lane", Float64MultiArray, callback_right_lane)
+    rospy.Subscriber("/steady_motion/enable", Bool, callback_enable_steady_motion)
+    rospy.Subscriber("/follow/enable", Bool, callback_enable_follow)
+    rospy.Subscriber("/obstacle/distance", Float64, callback_dist_to_obstacle)
     pub_speed = rospy.Publisher('/speed', Float64, queue_size=10)
     pub_angle = rospy.Publisher('/steering', Float64, queue_size=10)
     msg_left_lane  = rospy.wait_for_message('/demo/left_lane' , Float64MultiArray, timeout=10)
@@ -86,9 +101,19 @@ def main():
     print("Max speed: " + str(max_speed))
     print("K_rho: " + str(k_rho))
     print("K_theta: " + str(k_theta))
+    enable_steady_motion = False
+    enable_follow        = False
+    dist_to_obstacle     = 9.0
 
     while not rospy.is_shutdown():
-        speed, steering = calculate_control(lane_rho_l, lane_theta_l, lane_rho_r, lane_theta_r, goal_rho_l, goal_theta_l, goal_rho_r, goal_theta_r)
+        if enable_steady_motion:
+            speed, steering = calculate_control(lane_rho_l, lane_theta_l, lane_rho_r, lane_theta_r,
+                                                goal_rho_l, goal_theta_l, goal_rho_r, goal_theta_r)
+        elif enable_follow:
+            speed, steering = calculate_control(lane_rho_l, lane_theta_l, lane_rho_r, lane_theta_r,
+                                                goal_rho_l, goal_theta_l, goal_rho_r, goal_theta_r, dist_to_obstacle)
+        else:
+            continue
         pub_speed.publish(speed)
         pub_angle.publish(steering)
         rate.sleep()
